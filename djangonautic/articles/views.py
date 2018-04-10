6202dae1ea426950c392592c90e48393837c94a0
Natalia <natalia@miminotebook.local>
2018-04-10 16:05:26 +0200

    migrations, models, orm, admin, template tags

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def article_list(request):
-	return render(request, 'articles/article_list.html')
+	articles = Article.objects.all().order_by('date')
+	return render(request, 'articles/article_list.html', {'articles': articles})
